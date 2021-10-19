598064e929a0bec5b6dcec27d5831e10d671e3cb
Shivangi <shivangi54677@gmail.com>
2021-10-19 11:32:32 +0530

    all files added

---
+++ index.js
@@ -0,0 +1,9 @@
+  //  1.  Ways to print in a javascript
+
+  console.log("Hello World!");
+  alert("me");
+  document.write("This is a document write");
+      // 2.  Javascript console API: 
+        //console.log("Hello World!",4+6,"another log");
+        //console.warn("This is a warning");
+        //console.error("This is an error");
